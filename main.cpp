3f5ddf499cd43ee7b6c4dcb75abc0cb9ab8e5796
GAURAVSOMAN <gauravsoman25217@gmail.com>
2021-08-04 10:10:09 +0530

    First commit

--- main.cpp
+++ main.cpp
@@ -7,48 +7,72 @@
 using namespace std; 
 
 void delay();
+void admin_login();
+void student_info();
 
 int main()
 {
 
-    int number ;
-    cout << "This is an Attendance management system " << endl ;
-    cout << "Press :- "<<endl ;
-    cout << "1. Admin Login"<<endl ; 
-    cout << "2. Student Login" << endl ;
-    cin >> number ;
-
-    switch (number)
-    {
-    case 1:
-        system("clear");
-        delay();
-        cout << "Admin Login" << endl ;
-        break;
+    int number ,ite = 1,anum;
     
-    case 2 :
-        system("clear");
-        delay();
-        cout << "Studen Login" << endl ;
-        break;
-    default:
-        cout << "Enter a valid input "<<endl ;
+    while (ite > 0){
+        cout << "This is an Attendance management system " << endl ;
+        cout << "Press :- "<<endl ;
+        cout << "1. Admin Login"<<endl ; 
+        cout << "2. Student Login" << endl ;
+        cin >> number ;
+        switch (number)
+        {
+        case 1:
+            system("clear");
+            delay();
+            cout << "Admin Login" << endl ;
+            admin_login();
+            cout << "Login Successful" << endl ;
+            delay();
+            system("clear");
+            cout << "Press :- "<<endl ;
+            cout << "1. Student Info" << endl ; 
 
 
+            ite = 0 ;
+            break;
+
+    
+        case 2 :
+            system("clear");
+            delay();
+            cout << "Studen Login" << endl ;
+            break;
+        default:
+            cout << "Enter a valid input "<<endl ;
+}} }
+    
+void admin_login(){
+    string name ;
+    string password ;
+    cout << "Enter the admin username : -" ;
+    cin >> name ;
+    cout << "Enter the admin password : -" ;
+    cin >> password ;
+    if(name=="admin" && password=="admin@2"){
+        cout <<"Hello There "<<endl ;
     }
 
-
-
-}
-
-void admin_login(){
-    int name ;
-    cout << "Enter the admin username : -" ;
-    cin >> 
+    else {
+        cout << "The username or password is incorrect"<< endl ;
+    }
     
 }
 
+void student_info(){
+    fstream file ;
+    file.open("credentials.dat",ios::out);
+    
 
+
+
+}
 
 void delay(){
     for(int i = 0 ; i < 10 ; i ++ ){
