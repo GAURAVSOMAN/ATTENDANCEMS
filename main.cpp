4f3dcd44e465fa479c08498a306a5bb5cedc469b
GAURAVSOMAN <gauravsoman25217@gmail.com>
2021-08-04 19:23:05 +0530

    First Day

--- main.cpp
+++ main.cpp
@@ -2,24 +2,36 @@
 #include<string>
 #include<fstream>
 #include<stdlib.h>
-
-
 using namespace std; 
 
+bool student_login(string usname ,string pass);
 void delay();
-void admin_login();
+bool admin_login();
 void student_info();
+class Student {
+    string name ,username,password ;
+    int prn ;
 
+    public:
+    void takeinfo(string Name ,int roll,string un ,string pass ){
+        name = Name ;
+        prn = roll ;
+        username = un ;
+        password = pass ;
+    }
+
+
+};
 int main()
 {
-
     int number ,ite = 1,anum;
-    
+    string suname,spass ;
     while (ite > 0){
         cout << "This is an Attendance management system " << endl ;
         cout << "Press :- "<<endl ;
         cout << "1. Admin Login"<<endl ; 
         cout << "2. Student Login" << endl ;
+        cout << "3. To stop the program "<<endl ; 
         cin >> number ;
         switch (number)
         {
@@ -27,28 +39,43 @@ int main()
             system("clear");
             delay();
             cout << "Admin Login" << endl ;
-            admin_login();
-            cout << "Login Successful" << endl ;
-            delay();
-            system("clear");
-            cout << "Press :- "<<endl ;
-            cout << "1. Student Info" << endl ; 
-
-
+            if(admin_login()){
+                cout << "Login Successful" << endl ;
+                delay();
+            //system("clear");
+                cout << "Press :- "<<endl ;
+                cout << "1. Add Student " << endl ;
+                cout << "2. Remove Student " <<endl ;
+                cin >> anum ; 
+                switch (anum)
+                {
+                case 1:
+                    cout << "Adding Student " << endl;
+                    student_info();
+                    break; 
+                default:
+                    break;
+                }
+            }       
             ite = 0 ;
             break;
-
-    
         case 2 :
-            system("clear");
             delay();
-            cout << "Studen Login" << endl ;
+            system("clear");
+            cout <<"\t\tEnter Your Username And Password Below : -\n" ;
+            cout << "\nEnter our username :- " ;cin >> suname;
+            cout << "\nEnter your password :- "; cin >> spass ;
+            student_login(suname ,spass);
+            break;
+        case 3 :
+            ite = 0 ;
             break;
         default:
             cout << "Enter a valid input "<<endl ;
-}} }
-    
-void admin_login(){
+        }
+    } 
+}    
+bool admin_login(){
     string name ;
     string password ;
     cout << "Enter the admin username : -" ;
@@ -56,24 +83,50 @@ void admin_login(){
     cout << "Enter the admin password : -" ;
     cin >> password ;
     if(name=="admin" && password=="admin@2"){
-        cout <<"Hello There "<<endl ;
+        return true ;
     }
 
     else {
         cout << "The username or password is incorrect"<< endl ;
+        return false ;
     }
     
 }
-
 void student_info(){
     fstream file ;
-    file.open("credentials.dat",ios::out);
+    Student student ;
+    string a,b,c ; int d ;
+    cout << "Enter the students name : -" ;cin >> a  ;
+    cout << "Enter the students prn : -" ; cin >> d ;
+    cout << "Enter the students username : -" ;cin >> b ;
+    cout << "Enter the students password : -"; cin >> c ;
+
+    student.takeinfo(a,d,b,c);
+    file.open("studentinfo.dat",ios::app);
+    file << b << " "<< c << endl ;
     
-
-
 
 }
 
+bool student_login(string usname,string pass){
+    fstream file ;string word ,passwd ;
+    int i = 0 , j = 0 ; 
+    file.open("studentinfo.dat",ios::in);
+    while(file >> word){
+        i++ ;
+        if(word == usname){
+            break ;
+        } }
+    while(file >> word){
+        j++;
+        if(j < i ){
+            passwd = word;
+        }
+    }
+    cout << passwd;
+    
+    return true ;
+}
 void delay(){
     for(int i = 0 ; i < 10 ; i ++ ){
         for(int j = 0 ; j < 1000 ; j ++){
